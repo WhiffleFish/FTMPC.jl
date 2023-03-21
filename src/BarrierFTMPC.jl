a6ff04968fa200165b83003e6a1b198ff7e5507a
WhiffleFish <tylertronics@gmail.com>
2023-03-21 09:36:52 -0600

    ControlSystems -> ControlSystemsBase

--- src/BarrierFTMPC.jl
+++ src/BarrierFTMPC.jl
@@ -2,7 +2,7 @@ module BarrierFTMPC
 
 using LinearAlgebra
 using StaticArrays
-using ControlSystems
+using ControlSystemsBase
 using BlockArrays
 using SparseArrays
 using ProgressMeter
@@ -14,6 +14,9 @@ include("constants.jl")
 include("linear.jl")
 export LinearHexModel
 export flip_z, trans_states
+
+include("binary_search.jl")
+export binary_search_max
 
 include("failure_linearization.jl")
 export hover_control
